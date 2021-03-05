669aac39427bb5ba5f975c3cf35c2a01714bbb4b
websoft-ie <info@websoft.ie>
2021-03-05 00:36:06 +0000

    add new file

--- MBIPrimaryTest/Controllers/HomeController.cs
+++ MBIPrimaryTest/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MBIPrimaryTest.Models;
 using Newtonsoft.Json;
 
 namespace MBIPrimaryTest.Controllers
@@ -14,47 +15,30 @@ namespace MBIPrimaryTest.Controllers
             return View();
         }
 
-        
-        
-        [HttpPost]
-        public ActionResult Index(int max)
-        {
-            ViewBag.Primes = Util.GetPrimesUpTo(max);
-            return View();
-        }
-
         [HttpPost]
         public ActionResult GetMyList(int numberLimit)
         {
-            ViewBag.Primes = Util.GetPrimesUpTo(numberLimit);
-            return View("Index");
-        }
-    }
-
-    public static class Util
-    {
-        public static string GetPrimesUpTo(int max)
-        {
-            List<string> result = new List<string>();
-
-            for (int i = 1; i <= max; i++)
+            try
             {
-                bool isPrime = true;
-
-                for (int j = 2; j < i; j++)
+                if (numberLimit > 1)
                 {
-                    if (i % j == 0)
-                    {
-                        isPrime = false;
-                        break;
-                    }
+                    ViewBag.Status = true;
+                    var model = Util.GetPrimesUpTo(numberLimit);
+                    return View("Index", null, model);
                 }
-
-                if (isPrime)
-                    result.Add(i.ToString());
+                
+            }
+            catch (Exception)
+            {
+                // ignored
             }
 
-            return String.Join(",", result);
+            ViewBag.Status = false;
+            return View("Index");
+
+
         }
     }
+
+   
 }
