74374c9184b0c820bab06c5a51aa6235e3628ef3
websoft-ie <info@websoft.ie>
2021-03-05 01:23:03 +0000

    add

---
+++ MBIPrimaryTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MBIPrimaryTest.Models;
+using Newtonsoft.Json;
+
+namespace MBIPrimaryTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetMyList(int numberLimit= 1)
+        {
+            try
+            {
+                if (numberLimit > 1 && numberLimit<100001)
+                {
+                    ViewBag.Status = true;
+                    var model = Util.GetPrimesUpTo(numberLimit);
+                    return View("Index", null, model);
+                }
+                
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            ViewBag.Status = false;
+            return View("Index");
+
+
+        }
+    }
+
+   
+}
---
+++ MBIPrimaryTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MBIPrimaryTest.MvcApplication" Language="C#" %>
